19fb169ddad393c13b56e2b26cc5f36df1a4df0f
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-02-26 16:58:48 +0000

    Adds exercise 3 step 14

---
+++ directory_exercise_1-2_step_14.rb
@@ -0,0 +1,150 @@
+@students = [] # an empty array accessible to all methods
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp) # <- default for gets is STDIN only when no files are present as args
+  end
+end
+
+def print_menu
+  # print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list of students to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def add_students_to_array(students_hash) # <- exercise 1 (step 14)
+  @students << students_hash
+end
+
+def load_students(filename = "students.csv") # <- default arg if not provided
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    add_students_to_array({name: name, cohort: cohort.to_sym})
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # <- first argument from the command line
+  filename = "students.csv" if filename.nil? # <- exercise 2 (step 14)
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+def process(selection)
+  # do what the user has asked
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, please try again."
+  end
+end
+
+def input_students
+  puts "Please enter the student's name"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+  return if name.empty?
+
+  months = [
+    :Jan,
+    :Feb,
+    :Mar,
+    :Apr,
+    :May,
+    :Jun,
+    :Jul,
+    :Aug,
+    :Sep,
+    :Oct,
+    :Nov,
+    :Dec
+  ]
+  
+  puts "On which cohort is #{name}?"
+  cohort = STDIN.gets.chomp
+  cohort = "Mar (tbc)" if cohort == ""
+  while !months.include? cohort.to_sym
+    puts "Please enter cohort again (e.g. 'Mar')"
+    cohort = STDIN.gets.chomp
+  end
+  
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    add_students_to_array({name: name, cohort: cohort.to_sym})
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    puts "Enter the name of the next student"
+    name = STDIN.gets.chomp
+    break if name.empty?
+    puts "On which cohort is #{name}?"
+    cohort = STDIN.gets.chomp
+    cohort = "Mar (tbc)" if cohort == ""
+    while !months.include? cohort.to_sym
+      puts "Please enter cohort again (e.g. 'Mar')"
+      cohort = STDIN.gets.chomp
+    end
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  return if @students == nil
+  cohorts = @students.map { |student| student[:cohort] }
+  cohorts.uniq.each do |cohort|
+    puts "#{cohort} cohort members..."
+    @students.each do |student|
+      puts "#{student[:name]}" if student[:cohort] == cohort
+    end
+  end
+end
+
+def print_footer
+  return puts "Sadly we have no students right now" if @students == nil 
+  puts @students.count == 1 ?
+  "We have 1 great student" :
+  "Overall, we have #{@students.count} great students"
+end
+
+try_load_students
+interactive_menu
--- directory_exercise_3_step_14.rb
+++ directory_exercise_3_step_14.rb
@@ -3,12 +3,12 @@
 def interactive_menu
   loop do
     print_menu
-    process(STDIN.gets.chomp) # <- default for gets is STDIN only when no files are present as args
+    user_option(STDIN.gets.chomp) # <- default for gets is STDIN only when no files are present as args
   end
 end
 
 def print_menu
-  # print the menu and ask the user what to do
+  puts "What would you like to do?"
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list of students to students.csv"
@@ -47,18 +47,17 @@ end
 
 def try_load_students
   filename = ARGV.first # <- first argument from the command line
-  return if filename.nil?
+  filename = "students.csv" if filename.nil?
   if File.exists?(filename)
     load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
+    puts "Loaded #{@students.count} students from '#{filename}'"
   else
     puts "Sorry, #{filename} doesn't exist."
     exit
   end
 end
 
-def process(selection)
-  # do what the user has asked
+def user_option(selection)
   case selection
   when "1"
     input_students
@@ -76,74 +75,51 @@ def process(selection)
 end
 
 def input_students
-  puts "Please enter the student's name"
-  puts "To finish, just hit return twice"
-  name = STDIN.gets.chomp
-  return if name.empty?
-
   months = [
-    :Jan,
-    :Feb,
-    :Mar,
-    :Apr,
-    :May,
-    :Jun,
-    :Jul,
-    :Aug,
-    :Sep,
-    :Oct,
-    :Nov,
-    :Dec
+    :Jan, :Feb, :Mar, :Apr, :May, :Jun,
+    :Jul, :Aug, :Sep, :Oct, :Nov, :Dec
   ]
   
-  puts "On which cohort is #{name}?"
-  cohort = STDIN.gets.chomp
-  cohort = "Mar (tbc)" if cohort == ""
-  while !months.include? cohort.to_sym
-    puts "Please enter cohort again (e.g. 'Mar')"
-    cohort = STDIN.gets.chomp
-  end
-  
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    add_students_to_array({name: name, cohort: cohort.to_sym})
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    puts "Enter the name of the next student"
+  loop do
+    puts "Please enter student name (to return to menu, hit return twice)"
     name = STDIN.gets.chomp
     break if name.empty?
+
     puts "On which cohort is #{name}?"
     cohort = STDIN.gets.chomp
-    cohort = "Mar (tbc)" if cohort == ""
+    cohort = "Mar" if cohort == ""
+
     while !months.include? cohort.to_sym
       puts "Please enter cohort again (e.g. 'Mar')"
       cohort = STDIN.gets.chomp
     end
+    add_students_to_array({name: name, cohort: cohort.to_sym})
   end
 end
 
 def print_header
   puts "The students of Villains Academy"
-  puts "-------------"
+  puts "--------------------------------"
 end
 
 def print_student_list
-  return if @students == nil
+  return if @students.nil?
   cohorts = @students.map { |student| student[:cohort] }
   cohorts.uniq.each do |cohort|
     puts "#{cohort} cohort members..."
     @students.each do |student|
-      puts "#{student[:name]}" if student[:cohort] == cohort
+      puts "- #{student[:name]}" if student[:cohort] == cohort
     end
   end
 end
 
 def print_footer
-  return puts "Sadly we have no students right now" if @students == nil 
+  return puts "Sadly we have no students right now" if @students.nil? 
+  puts "--------------------------------"
   puts @students.count == 1 ?
   "We have 1 great student" :
   "Overall, we have #{@students.count} great students"
+  puts "--------------------------------"
 end
 
 try_load_students
